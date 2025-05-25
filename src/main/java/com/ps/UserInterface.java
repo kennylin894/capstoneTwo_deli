47ebbe33dbff0ea5615200b990d41ebbcc8f4d34
kennylin894 <Kennylin122004@gmail.com>
2025-05-24 20:22:51 -0400

    make the menu button work

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -5,19 +5,21 @@ import java.awt.*;
 
 public class UserInterface {
 
+    private static JFrame mainFrame;
     public static void initMainMenu()
     {
-        JFrame frame = new JFrame("DELI-cious - Main menu *Made By: Kenny*");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        mainFrame = new JFrame("DELI-cious - Main menu");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(600, 600);
+        mainFrame.setLocationRelativeTo(null);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (screenSize.width - frame.getWidth()) / 2;
-        int y = (screenSize.height - frame.getHeight()) / 2;
-        frame.setLocation(x, y);
+        int x = (screenSize.width - mainFrame.getWidth()) / 2;
+        int y = (screenSize.height - mainFrame.getHeight()) / 2;
+        mainFrame.setLocation(x, y);
 
         //making the background white
         Color logoBg = new Color(255, 255, 255);
-        frame.getContentPane().setBackground(logoBg);
+        mainFrame.getContentPane().setBackground(logoBg);
         JPanel menuPanel = new JPanel();
         menuPanel.setLayout(new GridLayout(7,1,10,20));
         menuPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
@@ -27,16 +29,12 @@ public class UserInterface {
         Image img = icon.getImage();
         Image scaledImg = img.getScaledInstance(300, 500, Image.SCALE_SMOOTH);
         JLabel imageLabel = new JLabel(new ImageIcon(scaledImg));
-        imageLabel.setOpaque(true);
-        imageLabel.setBackground(logoBg);
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.setBorder(BorderFactory.createEmptyBorder(10, 50, 30, 30));
         leftPanel.setBackground(logoBg);
-        leftPanel.add(Box.createVerticalGlue());
-        leftPanel.add(Box.createVerticalStrut(10));
         leftPanel.add(imageLabel);
-        frame.add(leftPanel, BorderLayout.WEST);
+        mainFrame.add(leftPanel, BorderLayout.WEST);
 
         //Buttons
         JButton menuButton = new JButton("View Menu");
@@ -60,6 +58,7 @@ public class UserInterface {
 
         //TODO: needs to display the menu/prices
         menuPanel.add(menuButton);
+        menuButton.addActionListener(e -> showMenuScreen());
 
         //TODO: needs to be able to add sandwich to cart including all the toppings (can be more than 1 topping)
         menuPanel.add(orderSandwichButton);
@@ -80,7 +79,117 @@ public class UserInterface {
         //TODO: exits the program
         menuPanel.add(exitButton);
         exitButton.addActionListener(e -> System.exit(0));
-        frame.add(menuPanel, BorderLayout.EAST);
-        frame.setVisible(true);
+        mainFrame.add(menuPanel, BorderLayout.EAST);
+        mainFrame.setVisible(true);
+    }
+
+    private static void showMenuScreen() {
+        mainFrame.getContentPane().removeAll();
+        JPanel foodMenuScreen = new JPanel(new BorderLayout());
+        foodMenuScreen.setBackground(Color.WHITE);
+
+        JLabel menuTitle = new JLabel("Menu",SwingConstants.CENTER);
+        menuTitle.setFont(new Font("SansSerif", Font.BOLD, 24));
+        foodMenuScreen.add(menuTitle, BorderLayout.NORTH);
+
+        // Scrollable content area
+        JTextArea menuTextArea = new JTextArea();
+        menuTextArea.setEditable(false);
+        menuTextArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
+
+        menuTextArea.setText("""
++--------------------------------------------------------------+
+|                   BUILD YOUR OWN SANDWICH                    |
++------------------+----------+----------+----------+----------+
+| Category         |   Item   |   4"     |   8"     |   12"    |
++------------------+----------+----------+----------+----------+
+| Base Sandwich    |          |  $5.50   |  $7.00   |  $8.50   |
++------------------+----------+----------+----------+----------+
+| Bread Choices    | White    |          |          |          |
+|                  | Wheat    |          |          |          |
+|                  | Rye      |          |          |          |
+|                  | Wrap     |          |          |          |
++------------------+----------+----------+----------+----------+
+| Meats            | Steak    |  $1.00   |  $2.00   |  $3.00   |
+|                  | Ham      |  $1.00   |  $2.00   |  $3.00   |
+|                  | Salami   |  $1.00   |  $2.00   |  $3.00   |
+|                  | Roast Bf |  $1.00   |  $2.00   |  $3.00   |
+|                  | Chicken  |  $1.00   |  $2.00   |  $3.00   |
+|                  | Bacon    |  $1.00   |  $2.00   |  $3.00   |
+| Extra Meat       |    +     |  $0.50   |  $1.00   |  $1.50   |
++------------------+----------+----------+----------+----------+
+| Cheese           | American |  $0.75   |  $1.50   |  $2.25   |
+|                  | Provolone|  $0.75   |  $1.50   |  $2.25   |
+|                  | Cheddar  |  $0.75   |  $1.50   |  $2.25   |
+|                  | Swiss    |  $0.75   |  $1.50   |  $2.25   |
+| Extra Cheese     |    +     |  $0.30   |  $0.60   |  $0.90   |
++------------------+----------+----------+----------+----------+
+| Toppings (Free)  | Lettuce  |          |          |          |
+|                  | Peppers  |          |          |          |
+|                  | Onions   |          |          |          |
+|                  | Tomatoes |          |          |          |
+|                  | Jalapeños|          |          |          |
+|                  | Cucumbers|          |          |          |
+|                  | Pickles  |          |          |          |
+|                  | Guac     |          |          |          |
+|                  | Mushrooms|          |          |          |
++------------------+----------+----------+----------+----------+
+| Sauces (Free)    | Mayo     |          |          |          |
+|                  | Mustard  |          |          |          |
+|                  | Ketchup  |          |          |          |
+|                  | Ranch    |          |          |          |
+|                  | 1000 Isl.|          |          |          |
+|                  | Vinaigret|          |          |          |
++------------------+----------+----------+----------+----------+
+| Sides (Free)     | Au Jus   |          |          |          |
+|                  | Sauce    |          |          |          |
++------------------+----------+----------+----------+----------+
+|                            Drinks                            |
++------------------+----------+----------+----------+----------+
+|         Sizes         |  Small     |  Medium    |  Large     |
++------------------+----------+----------+----------+----------+
+| Cola                  |  $2.00     |  $2.50     |  $3.00     |
+| Diet Cola             |  $2.00     |  $2.50     |  $3.00     |
+| Root Beer             |  $2.00     |  $2.50     |  $3.00     |
+| Lemon-Lime            |  $2.00     |  $2.50     |  $3.00     |
+| Orange Soda           |  $2.00     |  $2.50     |  $3.00     |
+| Iced Tea              |  $2.00     |  $2.50     |  $3.00     |
+| Lemonade              |  $2.00     |  $2.50     |  $3.00     |
+| Fruit Punch           |  $2.00     |  $2.50     |  $3.00     |
+| Ginger Ale            |  $2.00     |  $2.50     |  $3.00     |
+| Water                 |  $2.00     |  $2.50     |  $3.00     |
++------------------+----------+----------+----------+----------+
+|                            Chips                             |
++------------------+----------+----------+----------+----------+
+| Original             |  $1.50                                |
+| BBQ                  |  $1.50                                |
+| Sour Cream & Onion   |  $1.50                                |
+| Salt & Vinegar       |  $1.50                                |
+| Jalapeño             |  $1.50                                |
+| Cheddar Cheese       |  $1.50                                |
+| Lime                 |  $1.50                                |
+| Sea Salt             |  $1.50                                |
+| Kettle Cooked        |  $1.50                                |
+| Spicy Nacho          |  $1.50                                |
++------------------+----------+----------+----------+----------+
+""");
+
+        JScrollPane scrollPane = new JScrollPane(menuTextArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        foodMenuScreen.add(scrollPane, BorderLayout.CENTER);
+
+        JButton backButton = new JButton("Back");
+        backButton.setBackground(new Color(173, 216, 230));
+        backButton.addActionListener(e -> initMainMenu());
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(Color.WHITE);
+        bottomPanel.add(backButton);
+        foodMenuScreen.add(bottomPanel, BorderLayout.SOUTH);
+
+        mainFrame.getContentPane().add(foodMenuScreen);
+        mainFrame.revalidate();
+        mainFrame.repaint();
     }
 }
